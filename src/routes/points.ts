f50e9cdfec87a0022ee3b1d9672c3a236cd283b2
João Paulo Fontenele <joao.fontenele@linx3.com>
2020-06-07 11:31:48 -0400

    feat(routes/points): implement image upload for points

--- src/routes/points.ts
+++ src/routes/points.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import PointsController from '../controllers/points';
+import multerMiddleware from '../middlewares/multer';
 
 export const router = Router({ mergeParams: true });
 
 router.get('/points', PointsController.index);
 router.get('/points/:id', PointsController.show);
-router.post('/points', PointsController.create);
+router.post(
+  '/points',
+  multerMiddleware.single('image'),
+  PointsController.create,
+);
 
 export default router;
